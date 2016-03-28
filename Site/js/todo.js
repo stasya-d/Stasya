4560f4eafdcda8d714d67dc3bd5f424aa459271f
stasya-d <stasya.d@tut.by>
2016-03-28 22:48:26 +0300

    web

--- Site/js/todo.js
+++ Site/js/todo.js
@@ -1,18 +1,36 @@
 function run(){ 
 var appContainer = document.getElementsByClassName('content')[0]; 
 	appContainer.addEventListener('click', delegateEvent); 
-	/*appContainer.addEventListener('change', delegateEvent);*/
+	
 }
 
 function delegateEvent(evtObj) { 
 	if(evtObj.type === 'click' && evtObj.target.classList.contains('todo-button')){ 
 	myFunction(); 
 	} 
+	/*if(evtObj.type === 'click' && evtObj.target.classList.contains('delete-message')){ 
+	deleteRow(r); 
+	} 
+	if(evtObj.type === 'click' && evtObj.target.classList.contains('change-message')){ 
+	changeContent(r); 
+	} */
 }
-/*
-function myDeleteFunction() {
-    document.getElementById("myTable").deleteRow(0);
-}*/
+function deleteRow(r)
+{
+var i=r.parentNode.parentNode.rowIndex;
+document.getElementById('table').deleteRow(i);
+}
+function changeContent(r)
+{
+	var i=r.parentNode.parentNode.rowIndex;
+	var x=document.getElementById('table').rows[i].cells;
+	var d=new Date(); 
+	var text = document.getElementById("todoText"); 
+	/*text.value = x[1].innerHTML;*/
+	x[1].innerHTML=text.value +" Tекст был изменён "+d.getHours()+":"+d.getMinutes()+".";	
+	/*x[1].innerHTML +='<button id="delete-message" onclick="deleteRow(this)" class="delete-message"><button id="change-message" onclick="changeContent(this)" class="change-message">';
+*/
+}
 function myFunction(){ 
 	var text = document.getElementById("todoText"); 
 	if(text.value == "") return; 
@@ -21,19 +39,12 @@ function myFunction(){
 	var cell1 = row.insertCell(0); 
 	var cell2 = row.insertCell(1); 
 	var cell3 = row.insertCell(2); 
-	/*
-	var check = row.insertCell(3); 
-	var cell5 = row.insertCell(4); 
-	*/
+	var cell4 = row.insertCell(3); 
 	/*cell1.innerHTML = document.getElementById("you").value; */
 	cell1.innerHTML = "Kate"; 
 	cell2.innerHTML = text.value; 
-	
-	/*cell3.innerHTML = text.value; */
-	cell2.innerHTML +='<button id="delete-message" class="delete-message"><button id="change-message" class="change-message">';
-										
-	/*cell5.innerHTML ='<input type="button" value="/" onclick="changeRow(this)" class="chg">'; */
+	cell3.innerHTML ='<button id="delete-message" onclick="deleteRow(this)" class="delete-message"><button id="change-message" onclick="changeContent(this)" class="change-message">';
 	var d=new Date(); 
-	cell3.innerHTML=d.getHours()+":"+d.getMinutes(); 
+	cell4.innerHTML=d.getHours()+":"+d.getMinutes(); 
 	text.value=""; 
 }
